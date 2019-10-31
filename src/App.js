71277a737437d81aab9835b14a36a6879363f563
anurag <anuramy2729@gmail.com>
2019-11-01 02:13:58 +0530

    slider image added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "./components/headerandfooter/Header";
 import "./resources/styles.css";
+import Featured from "./components/featured";
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         }}
       >
         <Header />
+        <Featured />
       </div>
     );
   }
